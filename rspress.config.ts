afc840be07340420c4c8e979874c1b358feb90df
Manshawar <125840253+Manshawar@users.noreply.github.com>
2024-12-19 19:56:15 +0800

    feat: 合并忽略测试

--- rspress.config.ts
+++ rspress.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'rspress/config';
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   title: 'Manshawar blog',
-
+  base: '/Manshawar-blog/',
   icon: '/rspress-icon.png',
   builderConfig: {
     html: {
